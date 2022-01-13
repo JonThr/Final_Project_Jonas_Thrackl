424115c271184a0a6d6560cb16ab9d2947d11c12
Thrackl Jonas <thrackl.jonas@student.htlwrn.ac.at>
2022-01-13 23:10:44 +0100

    Add final_project

---
+++ final_project/contact-tracing-system-backend/models/userEntry.model.ts
@@ -0,0 +1,11 @@
+export interface UserEntry {
+    id: string;
+    qrCode: string;
+    firstname: string;
+    lastname: string;
+    company: string;
+    address: string;
+    city: string;
+    state: string;
+    postalCode: string;
+  }
